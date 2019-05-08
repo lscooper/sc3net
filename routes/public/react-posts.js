3d6557271264081e7f2a4585481fc4c1faa13f31
Laura SC <laura.shearer.cooper@gmail.com>
2019-05-07 19:54:11 -0600

    Removed some files, fiddling with babel

--- routes/public/react-posts.js
+++ routes/public/react-posts.js
@@ -1,12 +1,12 @@
 'use strict';
-var _posts = _interopRequireDefault(require("../../posts.json"));
+// var Posts = _interopRequireDefault(require("../../posts.json"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+// function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const e = React.createElement;
 
 class PostCard extends React.Component {
 
-  state ={ posts };
+ // state ={ Posts };
 
   render() {
     return (
@@ -14,11 +14,11 @@ class PostCard extends React.Component {
       
       e(
         "div", 
-        {class: "card-stacked"}, 
+        {className: "card-stacked"}, 
 
         e(
           "div", 
-          {class: "card-content"},
+          {className: "card-content"},
 
            e(
              "p", 
@@ -33,7 +33,7 @@ class PostCard extends React.Component {
         ), 
         e(
           "div", 
-          {class: "card-action"}, 
+          {className: "card-action"}, 
           e(
             "a", 
             {href: "#"}, 
@@ -45,12 +45,6 @@ class PostCard extends React.Component {
 
   }
 }
-
-
-// ReactDOM.render(
-//   <PostCard name="Steve" />,
-//   document.getElementById('#like_button_container')
-// );
 
 const domContainer2 = document.querySelector('#react-posts-div');
 ReactDOM.render(e(PostCard), domContainer2);
